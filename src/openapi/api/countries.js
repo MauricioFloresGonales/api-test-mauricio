c4781aaee314dcbcce7096620f4dea11b4dadbba
MauricioFloresGonales <mlgf2000@hotmail.com>
2020-04-15 15:43:34 -0300

    funcion put created in openApi

--- src/openapi/api/countries.js
+++ src/openapi/api/countries.js
@@ -38,45 +38,61 @@ module.exports = {
                 }
             }
         }
-        /*put: {
-            security: [],
-            summary: 'modify',
-            requestedBody: {
+    },
 
+    '/api/countries/'{id}: {
+        put: {
+            security: [],
+            summary: 'user id',
+            parameters: [
+                name: "id'
+                schema:
+                type: "string"
+                format: "uuid"
+            ]
+            requestedBody: {
+                description: 'modify'
+                content:'application/json':
+                schema:
+                type: 'object'
+                properties:{
+                    name: {type: "string"},
+                    code: {
+                        type: "string"
+                        maxLength: 2
+                    }
+                }
             }
             responses: {
-                200: {
-                    description: 'login success',
-                    content: {
-                        'application/json': {
-                            schema: {
-                                type: 'object',
-                                properties: {
-                                    countries: {
-                                        type: 'array',
-                                        items: {
-                                            type: 'object',
-                                            properties: {
-                                                id: {
-                                                    type: 'string',
-                                                    format: 'uuid'
-                                                },
-                                                name: {type: 'string'}
-                                            }
+                200: description: 'login success',
+                content: {
+                    'application/json': {
+                        schema: {
+                            type: 'object',
+                            properties: {
+                                country: {
+                                    type: 'array',
+                                    items: {
+                                        type: 'object',
+                                        properties: {
+                                            id: {
+                                                type: 'string',
+                                                format: 'uuid'
+                                            },
+                                            name: {type: 'string'}
                                         }
-                                    },
-                                    total: {type: 'integer'},
-                                    limit: {type: 'integer'}
+                                    }
                                 }
                             }
                         }
                     }
-                },
+                }
+            },
                 default: {
                     description: 'Error',
                     content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
                 }
             }
-        }*/
+        }
     }
 };
